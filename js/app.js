4d81b067a89c9a96098d4fa265b39b16d6458a52
arlez <rainmake123@gmail.com>
2021-09-29 13:44:55 -0300

    Primera version

---
+++ js/app.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    cargarPortafolio();
+});
+
+function cargarPortafolio(){
+    fetch('datos.json')
+    .then( (respuesta)=>{
+        return respuesta.json();
+    })
+    .then((datos)=>{
+        let html = '';
+        datos.portafolio.forEach(portafolio => {
+            //crear template
+            html+= `
+                <div class="elemento">
+                    <img src="img/${portafolio.id}.jpg" id="">
+                    <div class="contenido">
+                        <h3>${portafolio.nombre}</h3>
+                        <p>${portafolio.desc}</p>
+                    </div>
+                </div>
+            `;
+        });
+        //despues de crear el template, inyectar el html
+        document.querySelector('#listado').innerHTML = html;
+    })
+}
+
+const year = document.querySelector('.year');
+const date = new Date().getFullYear();
+year.textContent = date;
