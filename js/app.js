0dc2f4d37faa749384a87054c80669285521b722
arlez <rainmake123@gmail.com>
2021-10-23 19:04:19 -0300

    se agrego un proyecto nuevo a la seccion proyectos

--- js/app.js
+++ js/app.js
@@ -8,30 +8,33 @@ function cargarPortafolio(){
         return respuesta.json();
     })
     .then((datos)=>{
-        datos.portafolio.sort((a,b)=>{
-            return a-b;
-        });
-
+       /*if(datos.portafolio.length > 6){
+            const masProyectos = document.querySelector('.mas-proyectos');
+            masProyectos.classList.remove('d-none');
+        }*/
+        datos.portafolio.sort((a,b)=>{return a-b;});
         let html = '';
-        datos.portafolio.forEach(portafolio => {
+        datos.portafolio.forEach((portafolio,i)=> {
             //crear template
-            html+= `
-                <div class="col-md-6 col-xl-4 mt-4 portafolio Anime">
-                    <span class="demostracion">
-                        <span>${portafolio.tipo}</span>
-                    </span>
-                    <a href="${portafolio.link}" target="_blank" class="text-decoration-none text-dark">
-                        
-                        <figure>
-                            <img class="img/fluid" src="img/${portafolio.id}.png" id="${portafolio.id}" alt="imagen ${portafolio.nombre}">
-                            <div class="capa d-flex flex-column align-items-center justify-content-center">
-                                <h3 class="fw-bold text-center fs-2 text-white">${portafolio.nombre}</h3>
-                                <p class="m-0 text-center fs-5 text-white">${portafolio.desc}</p>
-                            </div>
-                        </figure>
-                    </a>
-                </div>
-            `;
+            if(i<=5){
+                html+= `
+                    <div class="col-md-6 col-xl-4 mt-4 portafolio Anime">
+                        <span class="demostracion">
+                            <span>${portafolio.tipo}</span>
+                        </span>
+                        <a href="${portafolio.link}" target="_blank" class="text-decoration-none text-dark">
+                            
+                            <figure>
+                                <img class="img/fluid" src="img/${portafolio.id}.png" id="${portafolio.id}" alt="imagen ${portafolio.nombre}">
+                                <div class="capa d-flex flex-column align-items-center justify-content-center">
+                                    <h3 class="fw-bold text-center fs-2 text-white">${portafolio.nombre}</h3>
+                                    <p class="m-0 text-center fs-5 text-white">${portafolio.desc}</p>
+                                </div>
+                            </figure>
+                        </a>
+                    </div>
+                `;
+            }
         });
         //despues de crear el template, inyectar el html
         document.querySelector('#listado').innerHTML = html;
